b9e59e4934443d1d56381f9817dc1ace6bb3683a
Cozma-Andrei <cozmaandrei1972@gmail.com>
2023-08-20 20:57:48 +0300

    Task 3 Completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,13 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  const authorBooks = {};
+  for (const key in books) {
+    if (books[key].author === author)
+      authorBooks[key] = books[key];
+  }
+  res.send(JSON.stringify(authorBooks, null, 4));
 });
 
 // Get all books based on title
