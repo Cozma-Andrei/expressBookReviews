50d25dd3688fd56d1cbfcac31335070886ed2555
Cozma-Andrei <cozmaandrei1972@gmail.com>
2023-08-21 20:25:34 +0300

    Task 7 Completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,43 +4,28 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-const doesExist = (username)=>{
-  let userswithsamename = users.filter((user)=>{
-    return user.username === username
-  });
-  if(userswithsamename.length > 0){
-    return true;
-  } else {
-    return false;
-  }
-}
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   if (username && password) {
-    if (!doesExist(username)) {
-      users.push({"username":username,"password":password});
-      return res.status(200).json({message: "User successfully registred. Now you can login"});
-    } else {
-      return res.status(404).json({message: "User already exists!"});
-    }
+    if (isValid(username))
+      return res.status(404).json({ message: "User already exists!" });
+    users.push({ "username": username, "password": password });
+    return res.status(200).json({ message: `User ${username} successfully registered.` });
   }
-  return res.status(404).json({message: "Unable to register user."});
+  return res.status(404).json({ message: "Unable to register user." });
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', function (req, res) {
   res.send(JSON.stringify(books, null, 4));
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', function (req, res) {
   let isbn = req.params.isbn;
   res.send(JSON.stringify(books[isbn], null, 4));
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+});
+
+public_users.get('/author/:author', function (req, res) {
   let author = req.params.author;
   const authorBooks = {};
   for (const key in books) {
@@ -50,8 +35,7 @@ public_users.get('/author/:author',function (req, res) {
   res.send(JSON.stringify(authorBooks, null, 4));
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', function (req, res) {
   let title = req.params.title;
   const book = {};
   for (const key in books) {
@@ -61,8 +45,7 @@ public_users.get('/title/:title',function (req, res) {
   res.send(JSON.stringify(book, null, 4));
 });
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get('/review/:isbn', function (req, res) {
   let isbn = req.params.isbn;
   res.send(books[isbn].reviews);
 });
